b724a9d0b8c7c571abc7689d6286b02461045000
aum battul <aumbattul99@gmail.com>
2023-08-25 19:14:31 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Spotify Clone",
+  description:
+    "A digital music service that gives you access to millions of songs.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
